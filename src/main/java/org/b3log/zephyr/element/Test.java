ad17b37553557d45e1437293bde13fe1097dee66
O型血橙 <zephyrjung@126.com>
2017-02-11 17:37:11 +0800

    :construction: 整合mybatis

---
+++ src/main/java/org/b3log/zephyr/element/Test.java
@@ -0,0 +1,43 @@
+package org.b3log.zephyr.element;
+
+/**
+ * Created by Zephyr on 2017/2/11.
+ */
+public class Test {
+    private String oId;
+    private String user_oId;
+    private String tag_oId;
+    private int type;
+
+    public String getoId() {
+        return oId;
+    }
+
+    public void setoId(String oId) {
+        this.oId = oId;
+    }
+
+    public String getUser_oId() {
+        return user_oId;
+    }
+
+    public void setUser_oId(String user_oId) {
+        this.user_oId = user_oId;
+    }
+
+    public String getTag_oId() {
+        return tag_oId;
+    }
+
+    public void setTag_oId(String tag_oId) {
+        this.tag_oId = tag_oId;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
