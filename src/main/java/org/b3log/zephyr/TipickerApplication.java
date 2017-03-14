f0e53842124789ff9f554f957ce6ea22a7b3c4fa
O型血橙 <zephyrjung@126.com>
2017-03-14 22:01:11 +0800

    :hammer: 弃用solr改用elasticsearch
    初期改框架就是这么容易。使用elastic search与论坛保持一致，减少学习负担

--- src/main/java/org/b3log/zephyr/TipickerApplication.java
+++ src/main/java/org/b3log/zephyr/TipickerApplication.java
@@ -1,17 +1,7 @@
 package org.b3log.zephyr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class TipickerApplication{
---
+++ src/main/java/org/b3log/zephyr/elasticsearch/CustomerRepository.java
@@ -0,0 +1,16 @@
+package org.b3log.zephyr.elasticsearch;
+
+/**
+ * Created by yaya on 17-3-14.
+ */
+import java.util.List;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface CustomerRepository extends ElasticsearchRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+
+    public List<Customer> findByLastName(String lastName);
+
+}
