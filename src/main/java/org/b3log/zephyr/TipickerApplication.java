8e8ecfbc1cee1470930c9d1d58979c823532dcca
O型血橙 <zephyrjung@126.com>
2017-03-20 17:51:54 +0800

    :construction: springboot websocket

--- src/main/java/org/b3log/zephyr/TipickerApplication.java
+++ src/main/java/org/b3log/zephyr/TipickerApplication.java
@@ -1,17 +1,7 @@
 package org.b3log.zephyr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class TipickerApplication{
@@ -19,6 +9,10 @@ public class TipickerApplication{
 //	@Bean
 //	public Queue queue() {
 //		return new ActiveMQQueue("sample.queue");
+//	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(TipickerApplication.class);
 //	}
 
 	public static void main(String[] args) {
